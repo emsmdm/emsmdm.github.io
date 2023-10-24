e33f7bcc13c3f97b38ea65672c99c0f7b35103b9
emsmith2000 <145351577+emsmdm@users.noreply.github.com>
2023-10-24 17:47:26 -0400

    email

--- validation.js
+++ validation.js
@@ -1,9 +1,41 @@
 function submissionCheck(){
-    function validateUsername(){
-        let usernameInput = document.infoForm.username;
-        if(usenameInput.value.match(/^[a-z0-9]*$/)){
-            true;
-            console.log(1);
-        }
+    const form = document.getElementById("fm");
+    let username = document.infoForm.username.value;
+    let email = document.infoForm.email.value;
+    if(username === ""){
+        let para = document.createElement("p");
+        let userError = document.createTextNode("Please Enter ");
+        let span = document.createElement("span");
+        span.style.color = "red";
+        span.innerText = "Username";
+        para.appendChild(userError);
+        para.appendChild(span);
+        form.appendChild(para);
+        return false;
+    } else if(username.match(/[a-z0-9]{4,12}/)) {
+        console.log("match");
+    } else{
+        let para = document.createElement("p");
+        let userError = document.createTextNode("Please Enter ");
+        let span = document.createElement("span");
+        span.style.color = "orange";
+        span.innerText = "a valid username";
+        para.appendChild(userError);
+        para.appendChild(span);
+        form.appendChild(para);
+        return false;
+    }
+    if(email === ""){
+        let paraOne = document.createElement("p");
+        let emailError = document.createTextNode("Please Enter ");
+        let span = document.createElement("span");
+        span.style.color = "red";
+        span.innerText = "Email";
+        paraOne.appendChild(emailError);
+        paraOne.appendChild(span);
+        form.appendChild(paraOne);
+        return false;
+    } else if(email.match(/[@](.net|.com|.org|.edu)$/)){
+
     }
 }
