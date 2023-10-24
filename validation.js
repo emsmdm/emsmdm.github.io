b142474eb3f9354ac1188fa56d8b11de7b2b90e8
emsmith2000 <145351577+emsmdm@users.noreply.github.com>
2023-10-23 22:28:55 -0400

    vali

--- validation.js
+++ validation.js
@@ -1,2 +1,8 @@
-let user = document.getElementById("username").innerHTML
-console.log(user);
+function submissionCheck(){
+    function validateUsername(){
+        let usernameInput = document.infoForm.username;
+        if(usenameInput.value.match(/^[a-z0-9]*$/)){
+            true;
+        }
+    }
+}
